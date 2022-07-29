8f45c0cab8e5c306691c79b7dc724f1ea52a5534
Dev Prumo <dev@prumoengenharia.com.br>
2022-07-29 13:00:33 -0300

    correção rotas

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompaniesController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::resource( 'companies', CompaniesController::class );
+Route::resource('companies', CompaniesController::class);
